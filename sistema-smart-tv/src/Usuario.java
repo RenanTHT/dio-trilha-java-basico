c3a045c2314230e52c43f21b0fa4979ea7621b5f
Renan <renan9854.rs@gmail.com>
2024-05-06 09:47:19 -0300

    Projeto SmatTV

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,34 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada ? " + smartTv.ligada);
+        smartTv.ligar();
+        System.out.println("TV ligada ? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal Atual: " + smartTv.canal);
+        
+        
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+
+
+
+
+
+    }
+}
