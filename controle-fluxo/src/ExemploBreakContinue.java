89f9e23a9f2deb4a2650fedef4ffba184e977e97
Renan <renan9854.rs@gmail.com>
2024-05-07 11:01:00 -0300

    Exemplo break e continue

---
+++ controle-fluxo/src/ExemploBreakContinue.java
@@ -0,0 +1,15 @@
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        
+        for(int numero = 1; numero <= 10; numero++){
+            if (numero == 8)
+                continue; //break or continue
+
+
+            System.out.println(numero);
+
+        }
+
+
+    }
+}
