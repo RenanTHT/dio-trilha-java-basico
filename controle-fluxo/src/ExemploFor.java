e5fd2a23e5248e625ad218ce2398260d2efed9a0
Renan <renan9854.rs@gmail.com>
2024-05-07 10:44:14 -0300

    Primeiro Comit ExemploFor

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        for(int carneirinhos = 0; carneirinhos <= 20; carneirinhos ++){
+            System.out.println("Contando Caneirinhos: " + carneirinhos);
+
+        }
+        System.out.println("Renan dormiu...");
+    }
+}
